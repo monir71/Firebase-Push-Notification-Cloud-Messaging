5cd60bedffe91c9bce1c4b6b1c071af0b7ce6db0
monir71 <zamanmonir71@yahoo.com>
2025-07-09 14:10:52 +0600

    Firebase Cloud Messaging Kotlin

---
+++ app/src/main/java/com/example/footballdilalpur/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.footballdilalpur
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful)
+            {
+                val token = task.result
+                Log.d("FirebaseToken", "Token: $token")
+            }
+            else
+            {
+                Log.w("FirebaseToken", "Fetching FCM registration token failed", task.exception)
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
